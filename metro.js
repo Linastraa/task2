c7ec0e232f8cb9dd8f35eaaa5644b92e57654269
Rintaruo <42626030+Rintaruo@users.noreply.github.com>
2022-07-31 12:34:47 +0300

    Add files via upload

---
+++ metro.js
@@ -0,0 +1,12 @@
+var slider = document.getElementById("servoSlider");
+    var servoP = document.getElementById("servoPos");
+    servoP.innerHTML = slider.value;
+    slider.oninput = function() {
+      slider.value = this.value;
+      servoP.innerHTML = this.value;
+    }
+    $.ajaxSetup({timeout:1000});
+    function servo(pos) {
+      $.get("/?value=" + pos + "&");
+    }
+    
